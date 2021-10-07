7085df42dde23b94a1a1cec86aa9a1029bf1ffea
vladebator <90964132+vladebator@users.noreply.github.com>
2021-10-07 21:12:26 +0300

    Update Main.java

--- src/com/onpy/Main.java
+++ src/com/onpy/Main.java
@@ -1,6 +1,14 @@
+/*
+Создать класс треугольник, члены класса – длины 3-х сторон. Предусмотреть в классе методы
+вычисления и вывода сведений о фигуре – длины сторон, углы, периметр, площадь. Создать
+производный класс – равнобедренный треугольник, предусмотреть в классе проверку, является
+ли треугольник равнобедренным. Написать программу, демонстрирующую работу с классом:
+дано N треугольников и M равнобедренных треугольников, найти среднюю площадь для N
+треугольников и равнобедренный треугольник с наименьшей площадью.
+*/
+
 package com.onpy;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 import static java.lang.Math.pow;
@@ -10,28 +18,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите количество треугольников: ");
-        int inCountTriangle = scan.nextInt();
+        int inCountTriangle = 10;//scan.nextInt();
 
-        triangle[] triangle = new triangle[inCountTriangle];
+        Triangle[] triangles = new Triangle[inCountTriangle];
         int CountTriangle = 0;
 
-        for (int i = 0; i < triangle.length; i++) {
-            triangle[i] = new triangle();
-            scan.nextLine();
+        double x1, x2, x3;
+        for (int i = 0; i < triangles.length/2; i++) {
+            do {
+                /*
+                System.out.print("Введите cторону Х1 для треугольника №" + i + ": ");
+                x1 = scan.nextInt();
+                System.out.print("Введите cторону Х2 для треугольника №" + i + ": ");
+                x2 = scan.nextInt();
+                System.out.print("Введите cторону Х3 для треугольника №" + i + ": ");
+                x3 = scan.nextInt();
+                 */
+                x1 = 1 + Math.random() * 10;
+                x2 = 1 + Math.random() * 10;
+                x3 = 1 + Math.random() * 10;
+            } while (!Triangle.isTriangle(x1, x2, x3));
 
-            triangle[i].numberTriangle = CountTriangle + 1;
+            triangles[i] = new Triangle(x1, x2, x3);
 
-            System.out.print("Введите cторону Х1 для треугольника №" + triangle[i].numberTriangle + ": ");
-            triangle[i].setX1(scan.nextInt());
-
-            System.out.print("Введите cторону Х2 для треугольника №" + triangle[i].numberTriangle + ": ");
-            triangle[i].setX2(scan.nextInt());
-
-            System.out.print("Введите cторону Х3 для треугольника №" + triangle[i].numberTriangle + ": ");
-            triangle[i].setX3(scan.nextInt());
-
-            CountTriangle = CountTriangle + 1;
+            //scan.nextLine();
+            triangles[i].numberTriangle = ++CountTriangle;
         }
+
+       // IsocelesTriangle[] isocelesTriangles = new IsocelesTriangle[inCountTriangle];
+
+        for (int i = triangles.length/2; i < triangles.length; i++) {
+            do {
+                x1 = 1 + Math.round(Math.random() * 10);
+                x2 = 1 + Math.round(Math.random() * 10);
+                x3 = 1 + Math.round(Math.random() * 10);
+            } while (!IsocelesTriangle.isIsoceles(x1, x2, x3));
+
+            triangles[i] = new IsocelesTriangle(x1, x2, x3);
+
+            //scan.nextLine();
+            triangles[i].numberTriangle = ++CountTriangle;
+        }
+        /*
 
         for (int i = 0; i < triangle.length; i++) {
             triangle[i].perimeter = triangle[i].x1 + triangle[i].x2 + triangle[i].x3;
@@ -47,7 +75,6 @@ public class Main {
             System.out.println("Косинус угла Betta = " + triangle[i].betta);
             System.out.println("Косинус угла Gamma = " + triangle[i].gamma);
 
-            // S = 0,5 * a * b⋅sin(α) , где a, b — стороны, α — угол между ними.
             triangle[i].square = 0.5 * triangle[i].x1 * triangle[i].x2 * Math.sin(triangle[i].alpha);
             System.out.println("Площадь треугольника №" + triangle[i].numberTriangle + " = " + triangle[i].square);
 
@@ -60,34 +87,42 @@ public class Main {
             }
         }
 
+
+         */
         double averageSquare = 0;
         double max = 0;
         double min = 0;
         int countNormalTriangle = 0;
         int countIsoscelesTriangle = 0;
 
-        for (int i = 0; i < triangle.length; i++) {
-            if (triangle[i].isosceles == 0) {
-                countNormalTriangle = countNormalTriangle + 1;
-                averageSquare = +triangle[i].square;
-            }
-            else {
-                countIsoscelesTriangle = countIsoscelesTriangle + 1;
 
-                for (int j = 0; j != triangle.length; j++) {
-                    if (!(triangle[i].square < max)) {
-                        max = triangle[i].square;
+        // Тут подправил. Сначала вычисляем для первой половины треугольников их площадь ( от 0, до triangles.length/2).
+        // В нашем решение, треугольник которые ( от 0, до triangles.length/2) - являются "обычными" (не равнобедренными)
+        // И в этом цикле я вычисляю среднюю площадь этих треугольников.
+        for (int i = 0; i < triangles.length/2; i++) {
+            countNormalTriangle = countNormalTriangle + 1;
+            averageSquare = +triangles[i].square;
+        }
+
+        // Тут подправил. В этом цикле находим минимальную площадь равнобедренного треугольника, которые
+        // ( от triangles.length/2 до triangles.length ) - Это вторая половина наших треугольников - равнобедренные.
+        for (int i = triangles.length/2; i < triangles.length; i++) {
+
+            countIsoscelesTriangle = countIsoscelesTriangle + 1;
+
+            for (int j = triangles.length/2; j != triangles.length; j++) {
+                    if (!(triangles[i].square < max)) {
+                        max = triangles[i].square;
                     }
-                    if (triangle[i].square < min) {
-                        min = triangle[i].square;
+                    if (triangles[i].square < min) {
+                        min = triangles[i].square;
                     }
                 }
             }
-        }
-
         averageSquare = averageSquare / countNormalTriangle;
-
+        // Выводим теперь эти данные на экран, среднюю площадь треугольников и мин.площ. равнобедренного треугольника.
         System.out.println("Средняя площадь треугольников = " + averageSquare);
         System.out.println("Минимальная площадь равнобедренного треугольника = " + min);
+
     }
 }
